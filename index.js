941aaf817f8d72d9aa5d3c1fd0f4ce6bfbe2e7a4
Vignesh K B <vigneshbala753@gmail.com>
2018-04-18 13:56:23 +0530

    mongo db changes

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+var mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect('mongodb://mern:mern@7ds149279.mlab.com:49279/mern-email',(err)=>{
+    if(err){
+        console.log('error occured',err);
+    }else{
+        console.log('connected to mongoDB');
+    }
+    
+});
 const authRoutes = require('./routes/authRoutes');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 //route in intialized
 authRoutes(app);
 app.listen(5000,()=>{
